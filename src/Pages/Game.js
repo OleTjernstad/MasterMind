e265f59c4d34ca09f0826555e1cd84ced14c742e
Ole Tjernstad <ott556@gmail.com>
2019-03-01 22:14:26 +0100

    import board in game

---
+++ src/Pages/Game.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Board from '../Components/Board'
+
+class Game extends Component {
+	render() {
+		return (<Board />);
+	}
+}
+
+export default Game;
