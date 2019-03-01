a7648fb6839191663ed39a341e114e792214d315
Ole Tjernstad <ott556@gmail.com>
2019-03-01 22:36:57 +0100

    add class

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,7 +6,7 @@ function Board(props){
   const rownums = [1,2,3,4,5,6,7,8,9,10,11,12]
 
   const Rows = rownums.map(num => <Row key={num} />)
-   return <div>{Rows}</div>
+   return <div className="board">{Rows}</div>
 }
 
 export default Board
