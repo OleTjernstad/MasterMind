f64225ae4585afd7b3e8e309066f9cd350cc112a
Ole Tjernstad <ott556@gmail.com>
2019-03-01 22:36:48 +0100

    imports Peg

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Peg from './Peg'
+import ScorePeg from './ScorePeg'
+import '../App.css';
 
 function Row(props){
-   return <div>Hello {props.name}</div>
+   return <div  className="row frame">Hello {props.name}</div>
 }
 
 export default Row
