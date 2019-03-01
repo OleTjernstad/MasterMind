a90accf6ea1aea9e0ad251501bbc022a38edab74
Ole Tjernstad <ott556@gmail.com>
2019-03-01 22:09:38 +0100

    map rows

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Row from 'Row'
 
 function Board(props){
-   return <div>Hello {props.name}</div>
+  const rownums = [1,2,3,4,5,6,7,8,9,10, 11, 12]
+
+  const Rows = rownums.map(num => <Row key="num" />)
+   return <Rows />
 }
 
 export default Board
