1cebbfb999b3a8ac5e2acbfc4ec78435cf88fa95
Ole Tjernstad <ott556@gmail.com>
2019-03-02 10:51:41 +0100

    return colorpicker

--- src/Components/ColorPicker.js
+++ src/Components/ColorPicker.js
@@ -2,13 +2,13 @@ import React from 'react'
 import '../App.css'
 
 function ColorPicker(props){
-   return <div class="colorSelect frame">
-     <div class="color colorRed"></div>
-     <div class="color colorCyan"></div>
-     <div class="color colorGreen"></div>
-     <div class="color colorOrange"></div>
-     <div class="color colorMagenta"></div>
-     <div class="color colorBlue"></div>
+   return <div className="colorSelect frame">
+     <div className="color colorRed"></div>
+     <div className="color colorCyan"></div>
+     <div className="color colorGreen"></div>
+     <div className="color colorOrange"></div>
+     <div className="color colorMagenta"></div>
+     <div className="color colorBlue"></div>
 
    </div>
 }
