c37fe2ea60c48a4d233dfd75381a852026b231a3
Ole Tjernstad <ott556@gmail.com>
2019-03-03 11:41:25 +0100

    beaufy code

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,11 +2,26 @@ import React from 'react'
 import Row from './Row'
 import '../App.css';
 
-function Board(props){
-  const rownums = [1,2,3,4,5,6,7,8,9,10,11,12]
+function Board(props) {
+	const rownums = [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12
+	]
 
-  const Rows = rownums.map(num => <Row key={num} />)
-   return <div className="board">{Rows}</div>
+	const Rows = rownums.map(num => <Row key={num}/>)
+	return <div>
+		<div className="board">{Rows}</div>
+	</div>
 }
 
 export default Board
