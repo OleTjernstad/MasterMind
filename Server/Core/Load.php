82430fadb918a42114a7ed40829d8b3aa0601eca
Ole Tjernstad <ott556@gmail.com>
2019-03-05 16:07:16 +0100

    error handling and load file

---
+++ Server/Core/Load.php
@@ -0,0 +1,4 @@
+<?php
+
+require dirname(__DIR__) . '/Core/Error.php';
+ ?>
---
+++ Server/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Front controller
+ *
+ * PHP version 7.0
+ */
+
+/**
+ * Composer
+ */
+require dirname(__DIR__) . '/Core/Load.php';
+
+
+/**
+ * Error and Exception handling
+ */
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+/**
+ * Sessions
+ */
+session_start();
