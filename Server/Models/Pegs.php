5a72e80b301201aa51c7086dd5e83f7bd813d132
Ole Tjernstad <ott556@gmail.com>
2019-03-06 18:19:18 +0100

    generate random colors

--- Server/Models/Pegs.php
+++ Server/Models/Pegs.php
@@ -2,7 +2,23 @@
 
 class Pegs
 {
-    public static function NewPegColors()
+    private $pegColors = [];
+    public function NewPegColors()
     {
+        for ($i=1; $i < 5 ; $i++) {
+            $key = 'peg' . $i;
+            $this->pegColors[$key] = $this->radomColor();
+        }
+
+        return $this->pegColors;
+    }
+    public function test()
+    {
+        return $this->pegColors;
+    }
+
+    private function radomColor()
+    {
+        return rand(1, 6);
     }
 }
