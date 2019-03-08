e7aebcd1fbfd1bd1471e5e126eb3123dccab0dae
Ole Tjernstad <ott556@gmail.com>
2019-03-08 21:55:14 +0100

    alow cors

--- Server/Controller.php
+++ Server/Controller.php
@@ -4,6 +4,12 @@ class Controller
 {
     public function App()
     {
+        $req_dump = print_r(json_decode(file_get_contents('php://input'), true), true);
+        $fp = fopen('request.log', 'a');
+        fwrite($fp, $req_dump);
+        fclose($fp);
+
+
         if (isset($_SESSION['pegs'])) {
             $pegs = $_SESSION['pegs'];
         } else {
@@ -14,6 +20,8 @@ class Controller
 
         $pegs->checkAnswer(['peg1' =>5, 'peg2' => 6, 'peg3' => 2, 'peg4' => 2]);
 
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
         header('Content-Type: application/json');
         echo json_encode($pegs);
         die();
