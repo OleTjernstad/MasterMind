0b4919d733157a299a527956ca181908e00d158e
Ole Tjernstad <ott556@gmail.com>
2019-03-09 23:11:28 +0100

    production link

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -198,7 +198,7 @@ class Board extends React.Component {
 	}
 
 	checkAnswerOnserver = (data) => {
-		fetch('http://mastermind.local/', {
+		fetch('https://olet.no/GC82P3Y/api/', {
 			method: 'POST',
 			headers: {
 				Accept: 'application/json',
