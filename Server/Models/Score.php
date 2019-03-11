59681cdf5d65751ccf4279548b378ed60ae0e94e
Ole Tjernstad <ott556@gmail.com>
2019-03-11 18:24:48 +0100

    update image on save status

--- Server/Models/Score.php
+++ Server/Models/Score.php
@@ -12,12 +12,14 @@ class Score
 
         $stmt->bindValue(':status', $status, PDO::PARAM_STR);
 
+        self::updateImage();
+
         return $stmt->execute();
     }
 
-    protected static function count()
+    protected static function count($status)
     {
-        $sql = 'SELECT COUNT(status)
+        $sql = 'SELECT COUNT(status) as status
                 FROM stilling
                 WHERE status=:status;';
 
@@ -26,18 +28,21 @@ class Score
 
         $stmt->bindValue(':status', $status, PDO::PARAM_STR);
 
-        return $stmt->execute();
+        $stmt->execute();
+        return $stmt->fetch();
     }
 
     public static function updateImage()
     {
+        $lose = self::count('lose');
+        $win = self::count('win');
         // FETCH IMAGE & WRITE TEXT
         $img = imagecreatefromjpeg('badge.jpg');
         $white = imagecolorallocate($img, 0, 0, 0);
 
         $font = "C:\www\master-mind\Server\\28-Days-Later.ttf";
-        imagettftext($img, 68, 0, 260, 230, $white, $font, 15);
-        imagettftext($img, 48, 0, 760, 305, $white, $font, 32);
+        imagettftext($img, 68, 0, 260, 230, $white, $font, $win['status']);
+        imagettftext($img, 48, 0, 760, 305, $white, $font, $lose['status']);
 
         // OUTPUT IMAGE
         // header('Content-type: image/jpeg');
