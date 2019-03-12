49e960308926fc29326e14d2151ed0c50896d7da
Ole Tjernstad <ott556@gmail.com>
2019-03-12 14:55:26 +0100

    accont for scroll

--- src/Components/ColorPicker.js
+++ src/Components/ColorPicker.js
@@ -2,34 +2,38 @@ import React from 'react'
 import ColorScheme from './ColorScheme'
 import '../App.css'
 
-function ColorPicker(props){
+function ColorPicker(props) {
 
-  const colorBlock = colIndex => {
-    return (<div className="color"
-        onClick={() => props.onClickColor(colIndex)}
-        style={{backgroundColor: ColorScheme[colIndex]}}>
-    </div>)
-  }
+	const colorBlock = colIndex => {
+		return (<div className="color" onClick={() => props.onClickColor(colIndex)} style={{
+				backgroundColor: ColorScheme[colIndex]
+			}}></div>)
+	}
+	let styles = {}
+	if (props.showColorPicker) {
+		let myTop = window.scrollY + props.colorPickerPlace.top + props.colorPickerPlace.height / 2
+		let myLeft = props.colorPickerPlace.right - props.colorPickerPlace.width / 2
+		styles = {
+			display: 'block',
+			top: myTop,
+			left: myLeft
+		}
 
-  if(props.showColorPicker)
-  {
-    var styles={display: 'block', top: props.colorPickerPlace.top, left: props.colorPickerPlace.left}
+	} else {
+		styles = {
+			display: 'none'
+		}
 
-  }
-  else {
-     styles={display: 'none'}
+	}
+	return <div style={styles} className="colorSelect frame">
+		{colorBlock(1)}
+		{colorBlock(2)}
+		{colorBlock(3)}
+		{colorBlock(4)}
+		{colorBlock(5)}
+		{colorBlock(6)}
 
-  }
-   return <div style={styles} className="colorSelect frame">
-     {colorBlock(1)}
-     {colorBlock(2)}
-     {colorBlock(3)}
-     {colorBlock(4)}
-     {colorBlock(5)}
-     {colorBlock(6)}
-
-
-   </div>
+	</div>
 }
 
 export default ColorPicker
